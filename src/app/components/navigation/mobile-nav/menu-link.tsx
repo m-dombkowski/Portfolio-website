003a46c1ba9893ac5f3e8e1c2f65fa9aec359c7a
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-02-05 22:18:56 +0100

    fix: fixed bug which was triggered by hovering to fast over an element. Switched framer to gsap

--- src/app/components/navigation/mobile-nav/menu-link.tsx
+++ src/app/components/navigation/mobile-nav/menu-link.tsx
@@ -1,56 +1,57 @@
-import { motion, useAnimate } from "framer-motion";
+import { motion } from "framer-motion";
 import { mountAnim, rotateX } from "../../../lib/anim";
 import Link from "next/link";
+import { clsx } from "clsx";
+import gsap from "gsap";
 import { NavDataType } from "@/app/lib/definitions/types";
-import { useRef } from "react";
+import { Dispatch, SetStateAction, useRef } from "react";
 import Image, { StaticImageData } from "next/image";
 
 export default function MenuLink({
   data,
   index,
+  toggleMenu,
+  setIsActive,
 }: {
   data: NavDataType;
   index: number;
+  toggleMenu: Dispatch<SetStateAction<boolean>>;
+  setIsActive: Dispatch<SetStateAction<boolean>>;
 }) {
   const { name, href, description, image } = data;
-  const [scope, animate] = useAnimate();
 
-  const outer = useRef(null);
+  const outer = useRef<HTMLDivElement>(null);
 
-  const inner = useRef(null);
+  const inner = useRef<HTMLDivElement>(null);
 
-  const animateIn = async (e: React.MouseEvent<HTMLElement>) => {
+  const manageMouseEnter = (e: React.MouseEvent<HTMLElement>) => {
     const bounds = e.currentTarget.getBoundingClientRect();
-    const direction = e.clientY < bounds.top + bounds.height / 2 ? -1 : 1;
-    await animate(
-      outer.current,
-      { top: `${direction * 100}%` },
-      { duration: 0 }
-    );
-    await animate(
-      inner.current,
-      { top: `${-1 * direction * 100}%` },
-      { duration: 0 }
-    );
-    animate([outer.current, inner.current], { top: "0%" }, { duration: 0.3 });
+    if (e.clientY < bounds.top + bounds.height / 2) {
+      gsap.set(outer.current, { top: "-100%" });
+      gsap.set(inner.current, { top: "100%" });
+    } else {
+      gsap.set(outer.current, { top: "100%" });
+      gsap.set(inner.current, { top: "-100%" });
+    }
+    gsap.to(outer.current, { top: "0%", duration: 0.3 });
+    gsap.to(inner.current, { top: "0%", duration: 0.3 });
   };
 
-  const animateOut = (e: React.MouseEvent<HTMLElement>) => {
+  const manageMouseLeave = (e: React.MouseEvent<HTMLElement>) => {
     const bounds = e.currentTarget.getBoundingClientRect();
-    const direction = e.clientY < bounds.top + bounds.height / 2 ? -1 : 1;
-    animate(outer.current, { top: `${direction * 100}%` }, { duration: 0.3 });
-    animate(
-      inner.current,
-      { top: `${-1 * direction * 100}%` },
-      { duration: 0.3 }
-    );
+    if (e.clientY < bounds.top + bounds.height / 2) {
+      gsap.to(outer.current, { top: "-100%", duration: 0.3 });
+      gsap.to(inner.current, { top: "100%", duration: 0.3 });
+    } else {
+      gsap.to(outer.current, { top: "100%", duration: 0.3 });
+      gsap.to(inner.current, { top: "-100%", duration: 0.3 });
+    }
   };
 
   return (
     <motion.div
-      onMouseEnter={animateIn}
-      onMouseLeave={animateOut}
-      ref={scope}
+      onMouseEnter={manageMouseEnter}
+      onMouseLeave={manageMouseLeave}
       variants={rotateX}
       {...mountAnim}
       custom={index}
@@ -58,21 +59,28 @@ export default function MenuLink({
     >
       <Link
         href={href}
+        onClick={() => {
+          setIsActive(false);
+          toggleMenu(false);
+        }}
         className="text-color-text-lighter uppercase text-[7vw] leading-[7.2vw] m-0"
       >
         {name}
       </Link>
       <div
         ref={outer}
-        className="flex absolute h-[100%] w-[100%] overflow-hidden pointer-events-none"
+        className="flex absolute h-[100%] w-[100%] overflow-hidden pointer-events-none will-change-[top]"
       >
         <div
           ref={inner}
-          className="flex bg-[#D3FD50] absolute whitespace-nowrap h-[100%] top-[100%]"
+          className="flex bg-[#D3FD50] absolute whitespace-nowrap h-[100%] top-[100%] "
         >
           {[...Array(2)].map((_, index) => {
             return (
-              <div key={index} className="opacity-0 group-hover:opacity-100 ">
+              <div
+                key={index}
+                className="flex items-center animate-[slider_14s_linear_infinite] transition-opacity duration-[500ms] relative opacity-0 group-hover:opacity-100"
+              >
                 <SliderContent description={description} image={image} />
               </div>
             );
@@ -90,7 +98,7 @@ const SliderContent = ({
   image: StaticImageData;
 }) => {
   return (
-    <div className="flex items-center animate-[slider_12s_linear_infinite] transition-opacity duration-300 relative ">
+    <>
       <div className="h-[6vw] w-[16vw] relative overflow-hidden rounded-[3vw] ml-[1vw] mr-[1vw] flex">
         <Image src={image} fill alt="image" className="object-cover" />
       </div>
@@ -103,6 +111,6 @@ const SliderContent = ({
       <p className="text-[#000] uppercase text-[7vw] leading-[7.2vw] m-0">
         {description}
       </p>
-    </div>
+    </>
   );
 };
