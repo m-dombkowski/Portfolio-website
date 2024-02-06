1adce11e5f156c898651c054a3d8bf97b5405bb4
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-02-06 23:36:27 +0100

    feat: temporary component call in work section while working on it

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,11 +1,17 @@
 "use client";
-import PageUnderContstuction from "../components/construction/construction";
+
+import ParallaxGallery from "../components/gallery/parallax-gallery/gallery";
 import PageWrapper from "../components/transitions/fade";
 
 export default function WorkPage() {
   return (
     <PageWrapper>
-      <PageUnderContstuction />
+      <section className="flex justify-center items-center flex-col">
+        <div className="max-w-[600px] xl:max-w-[900px] flex justify-center flex-col items-center font-calc">
+          <h1 className="text-4xl pb-10 sm:text-6xl">Day to day tech stack</h1>
+        </div>
+        <ParallaxGallery />
+      </section>
     </PageWrapper>
   );
 }
