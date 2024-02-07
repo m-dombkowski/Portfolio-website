d8e20db9ba9d1ec5a4d961bd25592463c1d1dc6b
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-02-07 23:01:08 +0100

    docs: removed Parallax Gallery from "testing envirnoment"

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -10,7 +10,7 @@ export default function WorkPage() {
         <div className="max-w-[600px] xl:max-w-[900px] flex justify-center flex-col items-center font-calc">
           <h1 className="text-4xl pb-10 sm:text-6xl">Day to day tech stack</h1>
         </div>
-        <ParallaxGallery />
+        {/* <ParallaxGallery /> */}
       </section>
     </PageWrapper>
   );
