daeda0feca27ec5ade167da549592dc0957e42b1
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-02-10 23:56:46 +0100

    docs: added tooltip data for tech-stack-data objects

--- src/app/lib/data/tech-stack-data.ts
+++ src/app/lib/data/tech-stack-data.ts
@@ -4,53 +4,71 @@ export const techData: TechStackDataType[] = [
   {
     name: "Next.js",
     usecase: ["personal"],
+    tooltip: "0.5 year",
   },
   {
     name: "React",
     usecase: ["commercial", "personal"],
+    tooltip: "2 years",
   },
   {
     name: "Typescript",
     usecase: ["commercial", "personal"],
+    tooltip: "1.5 years",
   },
   {
     name: "Redux (Toolkit)",
     usecase: ["commercial", "personal"],
+    tooltip: "1.5 years",
   },
   {
     name: "REST Api",
     usecase: ["commercial", "personal"],
+    tooltip: "1.5 years",
   },
   {
     name: "Javasript",
     usecase: ["commercial", "personal"],
+    tooltip: "2 years",
   },
   {
     name: "CSS",
-    usecase: ["commercial", "personal"],
+    usecase: ["commercial"],
+    tooltip: "2 years",
   },
   {
     name: "SCSS",
     usecase: ["commercial", "personal"],
+    tooltip: "1.5 ys",
+  },
+  {
+    name: "Tailwind",
+    usecase: ["personal"],
+    tooltip: "0.5 y",
   },
   {
     name: "HTML",
     usecase: ["commercial"],
+    tooltip: "2 years",
   },
   {
     name: "Git",
     usecase: ["commercial", "personal"],
+    tooltip: "2 years",
   },
   {
     name: "Supabase",
     usecase: ["personal"],
+    tooltip: "0.5 year",
   },
   {
     name: "Magento",
     usecase: ["commercial"],
+    tooltip: "1.5 years",
   },
   {
     name: "Firebase",
     usecase: ["personal"],
+    tooltip: "2 years",
   },
 ];
