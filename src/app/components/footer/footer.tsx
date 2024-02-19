7d94463550430c1e9fb7faa3857e67f01a29f29e
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-02-19 23:31:47 +0100

    feat: a lot of footer work, basically whole component from scratch. Needs to be cleaned, some code put into different smaller components

--- src/app/components/footer/footer.tsx
+++ src/app/components/footer/footer.tsx
@@ -1,19 +1,173 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "lucide-react";
+import Link from "next/link";
 import SpotifyLink from "../spotify-link/spotify-link";
-import { faSpotify } from "@fortawesome/free-brands-svg-icons";
+import { faSpotify } from "@fortawesome/free-brands-svg-icons/faSpotify";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/app/components/ui/hover-card";
+import {
+  NextSvg,
+  TailwindSvg,
+  TypescriptSvg,
+  VercelSvg,
+} from "@/app/lib/data/tech-stack-svgs";
+import { Typewriter } from "react-simple-typewriter";
+
+const links = [
+  {
+    name: "CV",
+    href: "#",
+  },
+  {
+    name: "Email",
+    href: "mailto:dombkowskimateusz@gmail.com",
+  },
+  {
+    name: "Github",
+    href: "https://github.com/m-dombkowski",
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/mateusz-dombkowski-683b26241/",
+  },
+
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/mateusz.dombkowski/",
+  },
+];
+
+const techstack = [
+  {
+    name: "Code",
+    technology: "Next.js",
+    icon: NextSvg(),
+  },
+  {
+    name: "Styling",
+    technology: "Tailwind",
+    icon: TailwindSvg(),
+  },
+  {
+    name: "Types",
+    technology: "Typescript",
+    icon: TypescriptSvg(),
+  },
+  {
+    name: "Launch",
+    technology: "Vercel",
+    icon: VercelSvg(),
+  },
+];
 
 export default function Footer() {
   return (
-    <div className="w-[250px] bg-bg-dark-gray h-[75px] flex justify-center items-center gap-5 text-sm sm:w-[375px] sm:gap-8 sm:h-[120px] sm:text-xl">
-      <Link
-        target="_blank"
-        href="https://open.spotify.com/user/hevrak?si=3764935cfac041dd"
-      >
-        {" "}
-        <FontAwesomeIcon size="3x" color="#8fdcc2" icon={faSpotify} />
-      </Link>
-      <SpotifyLink />
-    </div>
+    <footer className="m-auto flex justify-center items-center py-32 gap-20 flex-col xl:flex-row xl:gap-32 xl:items-start">
+      <div className="flex flex-col gap-10 items-center">
+        <div className="w-[250px] bg-bg-dark-gray h-[75px] flex justify-center items-center gap-5 text-sm sm:w-[350px] sm:gap-12 sm:h-[120px] sm:text-xl rounded-full">
+          <Link
+            target="_blank"
+            href="https://open.spotify.com/user/hevrak?si=3764935cfac041dd"
+          >
+            {" "}
+            <FontAwesomeIcon size="3x" color="#8fdcc2" icon={faSpotify} />
+          </Link>
+
+          <div className="text-xs sm:text-sm lg">
+            <span className="pb-1 inline-block">
+              On repeat
+              <Typewriter
+                words={[" ..."]}
+                loop={false}
+                typeSpeed={150}
+                deleteSpeed={150}
+              />
+            </span>
+            <div>
+              <SpotifyLink />
+            </div>
+          </div>
+        </div>
+        <div className="flex  items-center gap-5 justify-center text flex-col sm:flex-row">
+          <p className="text-base sm:text-xl">© 2024 Mateusz Dombkowski</p>
+          <div>
+            <HoverCard>
+              <HoverCardTrigger className="underline cursor-pointer">
+                Technologies
+              </HoverCardTrigger>
+              <HoverCardContent>
+                <h1 className="text-xl mb-5 text-center">
+                  Technologies and tools used to build this website
+                </h1>
+                <div className="flex gap-10 flex-col sm:flex-row">
+                  <ul className="flex flex-col justify-between gap-4 text-base ">
+                    {techstack.map((tech, index) => {
+                      return (
+                        <li
+                          key={index}
+                          className="flex items-center gap-3 w-[200px]"
+                        >
+                          <div className="p-2 bg-[#fff] inline-block rounded-full">
+                            <div className="w-[20px] h-[20px] ">
+                              {tech.icon}
+                            </div>
+                          </div>
+                          <span className="my-auto">
+                            {`${tech.name}: ${tech.technology}`}
+                          </span>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                  <div className="flex flex-col gap-5 text-lg">
+                    <p>
+                      CalcSans and Inter used as fonts. Animations made with
+                      Framer Motion and Green Sock a.k.a GSAP.
+                    </p>
+                    <p>
+                      For detailed information and code check{" "}
+                      <Link
+                        target="_blank"
+                        href="https://github.com/m-dombkowski/Portfolio-website"
+                      >
+                        repository
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </HoverCardContent>
+            </HoverCard>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col gap-20 lg:flex-row">
+        <div className="flex flex-col items-center lg:items-start">
+          <h1 className="text-3xl mb-10">Contact info</h1>
+          <ul className="flex flex-col gap-5 items-center lg:items-start">
+            {links.map((link, index) => {
+              return (
+                <li key={index} className="text-xl">
+                  <Link
+                    href={link.href}
+                    className="text-[#fff] transition duration-300 hover:text-color-text-primary"
+                    target="_blank"
+                  >
+                    {link.name}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <div className="max-w-[500px] px-10 lg:px-0">
+          <h1 className="text-3xl mb-10 text-center">Recruitment status</h1>
+          <p className="text-sm text-justify leading-8 sm:text-lg">
+            {`Always opened and happy for a chat, if you're looking for a Frontend Developer for a role on site or hybrid model in Wroclaw/Krakow or remotely anywhere in Poland feel free to contact me the way that suits you best. I'm always open for a new opportunities so if the work would be in any other "big" polish city or abroad I will consider reloacation.`}
+          </p>
+        </div>
+      </div>
+    </footer>
   );
 }
