cc13c1a49cdd1f4918259bef77ae3fdb46fcc5d5
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-02-27 19:19:36 +0100

    fix: fixed mask image gradient on hover

--- src/app/components/card/card.tsx
+++ src/app/components/card/card.tsx
@@ -12,26 +12,25 @@ export const Card: React.FC<PropsWithChildren> = ({ children }) => {
     mouseX.set(clientX - left);
     mouseY.set(clientY - top);
   }
-  const maskImage = useMotionTemplate`radial-gradient(240px at ${mouseX}px ${mouseY}px, #fff, transparent)`;
+  const maskImage = useMotionTemplate`radial-gradient(200px at ${mouseX}px ${mouseY}px, white, transparent)`;
   const style = { maskImage, WebkitMaskImage: maskImage };
 
   return (
     <div
       onMouseMove={onMouseMove}
-      className="overflow-hidden relative duration-700 border rounded-xl hover:bg-zinc-800/10 group md:gap-8 hover:border-zinc-400/50 border-white"
+      className="group overflow-hidden relative duration-700 border rounded-xl border-white md:gap-8 bg-[transparent]"
     >
       <div className="pointer-events-none">
-        <div className="absolute inset-0 z-0  transition duration-1000 [mask-image:linear-gradient(black,transparent)]" />
+        <div className="absolute inset-0 transition duration-500 [mask-image:linear-gradient(white,transparent)]" />
         <motion.div
-          className="absolute inset-0 z-10  bg-gradient-to-br opacity-100  via-zinc-100/10  transition duration-1000 group-hover:opacity-50 "
+          className="absolute inset-0 bg-gradient-to-br opacity-20 from-[#d4ede4] via-[#7bbea8] to-[#8fdcc2]  transition duration-500 group-hover:opacity-30"
           style={style}
         />
         <motion.div
-          className="absolute inset-0 z-10 opacity-0 mix-blend-overlay transition duration-1000 group-hover:opacity-100"
+          className="absolute inset-0 opacity-0 mix-blend-overlay group-hover:opacity-100"
           style={style}
         />
       </div>
-
       {children}
     </div>
   );
