f382f758a1d857f658ad564a4703277787229d6f
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-02-28 21:02:54 +0100

    styles: small animation for hover state

--- src/app/components/project-card/project-card.tsx
+++ src/app/components/project-card/project-card.tsx
@@ -19,18 +19,21 @@ export default function ProjectCard({
   return (
     <>
       <Card>
-        <Link href={href} target="_blank">
+        <Link href={href} target="_blank" className="group">
           <div className="flex flex-col justify-start items-start  flex-wrap text-color-text-lighter h-full">
             <span className="pt-10 px-10 mb-5">{date}</span>
-            <h1 className="px-10 text-4xl mb-7 font-calc text-color-text-lighter">
+            <h1 className="px-10 text-4xl mb-7 font-calc text-color-text-primary">
               {projectTitle}
             </h1>
             <p className="px-10 mb-10 text-lg  sm:text-base text-color-text-lighter">
               {description}
             </p>
             {bottomLink && (
-              <p className="px-10 mt-auto mb-10 text-color-text-lighter">
-                Read more →
+              <p className="relative pl-10 mt-auto mb-10 text-color-text-lighter">
+                Read more{" "}
+                <span className="absolute right-4 transition-[right] duration-300 group-hover:right-2">
+                  →
+                </span>
               </p>
             )}
           </div>
