534e3579163d4c5150682ffc501ada5359bb8d80
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-03-02 17:50:42 +0100

    feat: helper function for getting random argument from any type of array

--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function getRandomFromArray<T>(givenArray: Array<T>): T {
+  return givenArray[(Math.random() * givenArray.length) | 0];
+}
