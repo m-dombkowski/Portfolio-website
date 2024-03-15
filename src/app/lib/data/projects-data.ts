1e9e7913056d87800df9c185fff57760100c6f83
Mateusz Dombkowski <dombkowskimateusz@gmail.com>
2024-03-15 23:55:24 +0100

    docs: updating project data with links to website and repo

--- src/app/lib/data/projects-data.ts
+++ src/app/lib/data/projects-data.ts
@@ -22,28 +22,28 @@ export const projectData: ProjectCardType[] = [
       "Framer Motion",
       "GSAP",
     ],
-    repoLink: "#",
-    websiteLink: "#",
+    repoLink: "https://github.com/m-dombkowski/Portfolio-website",
+    websiteLink: "https://www.dombkowski.pl",
   },
-  {
-    date: "Work in progress",
-    projectTitle: "Moc grzybiarzy",
-    briefDescription:
-      "Weather checking application with beautiful visuals, database connection, account where user can bookmark his favorite cities.",
-    gifs: ["https://i.ibb.co/TBB0YH2/vid-1.gif"],
-    alt: `Bafi's pic`,
-    description:
-      "Application where users are able to check weather, bookmark their favorites cities and also check more specific data like forecast, time, humidity, pressure, sunset time, air quality and many more. Powered by OpenWeather API and Supabase Postgres SQL Database.",
-    technologies: [
-      "Next",
-      "React",
-      "Typescript",
-      "Openweather API",
-      "Tailwind",
-      "Supabase",
-      "React Hook Form",
-    ],
-    repoLink: "#",
-    websiteLink: "#",
-  },
+  // {
+  //   date: "Work in progress",
+  //   projectTitle: "Moc grzybiarzy",
+  //   briefDescription:
+  //     "Weather checking application with beautiful visuals, database connection, account where user can bookmark his favorite cities.",
+  //   gifs: ["https://i.ibb.co/TBB0YH2/vid-1.gif"],
+  //   alt: `Bafi's pic`,
+  //   description:
+  //     "Application where users are able to check weather, bookmark their favorites cities and also check more specific data like forecast, time, humidity, pressure, sunset time, air quality and many more. Powered by OpenWeather API and Supabase Postgres SQL Database.",
+  //   technologies: [
+  //     "Next",
+  //     "React",
+  //     "Typescript",
+  //     "Openweather API",
+  //     "Tailwind",
+  //     "Supabase",
+  //     "React Hook Form",
+  //   ],
+  //   repoLink: "#",
+  //   websiteLink: "#",
+  // },
 ];
